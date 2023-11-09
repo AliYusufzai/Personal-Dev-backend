47948f7dbcce19863fe9f9e372020298f8c350f2
Ali Raza Yusufzai <aliyusufzai03@gmail.com>
2023-11-09 13:35:40 +0500

    .

---
+++ api/user/controller.js
@@ -0,0 +1,6 @@
+const UserDetails = require("../../model/userDetails");
+const Users = require("../../model/user");
+const CareerInfo = require("../../model/careerInfo");
+const PersonalInfo = require("../../model/personalInfo");
+
+module.exports = {};
